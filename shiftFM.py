2cdce750be5b74b9fca9a179af47a17425760e44
Scott Breakall <breakall@gmail.com>
2021-11-23 14:43:14 -0500

    Update shiftFM.py

--- shiftFM.py
+++ shiftFM.py
@@ -1,4 +1,4 @@
-# fmshift.py
+# shiftFM.py
 # parameters: [frequency in MHz] [recording duration in seconds] [name of program]
 # example: python3 fmshift.py 96.1 News---> News_10-04-2020.mp3 
 # recording will be terminated after the specified duration
